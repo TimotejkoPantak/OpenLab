a83261cfcc5cb9111e86480cabec64ed35ad3cc5
Timotej Panták <timopantakgit@gmail.com>
2021-09-14 08:28:21 +0200

    Edit text

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,6 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2021!");
+        System.out.println("Hello AppsLab 2021!" );
     }
 }
